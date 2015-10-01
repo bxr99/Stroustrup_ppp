8bae6b9cadbf4046678779c70f78b1c2411e1269
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-01 09:40:44 -0400

    started chapter 2

---
+++ Chapter_2/helloWorld.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main() // C++ programs start by executing main
+{
+	cout << "Hello, World!\n"; // output "Hello, World!"
+	return 0;
+}
+
+// To compile $ g++ -Wall -std=c++11 fileName.cpp -o fileName
