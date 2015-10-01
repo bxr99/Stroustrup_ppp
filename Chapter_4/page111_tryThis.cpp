62fccfca5e09cc233415c88bb1ee3dc18cce6967
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-01 17:05:38 -0400

    try-this exercise from page 111

---
+++ Chapter_4/page111_tryThis.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+
+// Prints a table of characters with their corresponding integer values
+int main()
+{
+  // 'b' is char('a' + 1), 'c' is char('a' + 2)
+	int i = 0;
+	int char_val = char('a' + i);
+	while (char_val <= 122) {
+		cout << char('a' + i) << '\t' << char_val << '\n';
+		++i;
+		char_val = char('a' + i);
+	}
+  return 0;
+}
