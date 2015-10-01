8857cf52755dc3e71cf11b2f7ae21b90cbc7f95a
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-01 18:06:20 -0400

    try this exercise

---
+++ Chapter_4/tryThis_pg125.cpp
@@ -0,0 +1,20 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	vector<string> words;
+	string disliked = "broccoli";
+
+	int i = 0;
+	for (string input; cin >> input;) {
+		words.push_back(input);
+		if (input == disliked) {
+			cout << "BLEEP\n";
+		  ++i;
+		} else {
+			cout << words[i++] << '\n';
+		}
+	}
+
+	return 0;
+}
