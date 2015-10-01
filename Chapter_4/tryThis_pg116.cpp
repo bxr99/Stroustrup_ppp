0796e7c36a7c4e496afe616b94c816c14b1bc96c
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-01 17:41:19 -0400

    changed file names

---
+++ Chapter_4/tryThis_pg116.cpp
@@ -0,0 +1,20 @@
+#include "std_lib_facilities.h"
+
+// Implementation of square() without using the multiplication operator
+int square(int n)
+{
+	int res = n;
+	for (int i = 1; i < n; ++i) {
+		res += n;
+	}
+	return res;
+}
+
+int main()
+{
+	int n;
+	cout << "Enter an integer to square: ";
+	cin >> n;
+	cout << n << " * " << n << " = " << square(n) << '\n';
+	return 0;
+}
