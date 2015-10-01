0cc96b2831c25deb6ae76f2aad52365835304664
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-01 17:23:44 -0400

    prints table of uppercase characters and their ascii values

---
+++ Chapter_4/page113_tryThis.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+
+// Prints a table of uppercase characters with their corresponding integer values
+int main()
+{
+	int i = 0;
+	for (int char_val = char('A' + i); char_val <= 90; ++char_val) {
+		cout << char('A' + i++) << '\t' << char_val << '\n';
+	}
+	return 0;
+}
