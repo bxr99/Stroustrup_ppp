eea75da3b2f27f66e0d04965735a5714932017ea
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 15:44:26 -0400

    fixed spacing

--- Chapter_3/exercise_10.cpp
+++ Chapter_3/exercise_10.cpp
@@ -2,20 +2,20 @@
 
 int main()
 {
-	string operation;
-	cin >> operation;
+  string operation;
+  cin >> operation;
 
-	double v1, v2; // values
-	cin >> v1 >> v2;
+  double v1, v2; // values
+  cin >> v1 >> v2;
 
-	if (operation == "+")
-		cout << v1 + v2 << '\n';
-	if (operation == "-")
-		cout << v1 - v2 << '\n';
-	if (operation == "*")
-		cout << v1 * v2 << '\n';
-	if (operation == "/")
-		cout << v1 / v2 << '\n';
+  if (operation == "+")
+    cout << v1 + v2 << '\n';
+  if (operation == "-")
+    cout << v1 - v2 << '\n';
+  if (operation == "*")
+    cout << v1 * v2 << '\n';
+  if (operation == "/")
+    cout << v1 / v2 << '\n';
 
-	return 0;
+  return 0;
 }
