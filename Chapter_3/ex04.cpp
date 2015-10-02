ca5a8ea067ad0093b17f396c3b8e7ca1dcfbe53e
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 19:28:44 -0400

    chapter 4 exercise 4

---
+++ Chapter_3/ex04.cpp
@@ -0,0 +1,27 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+  int val1 = 0;
+  int val2 = 0;
+
+  cout << "Enter two integer values (separated by <space>): ";
+  cin >> val1 >> val2;
+
+  if (val1 < val2) {
+    cout << "the smaller value is " << val1 << '\n';
+    cout << "the larger value is " << val2 << '\n';
+  } else if (val1 == val2) {
+    cout << "the two values are equal\n";
+  } else { // val1 > val2
+    cout << "the larger value is " << val1 << '\n';
+    cout << "the smaller value is " << val2 << '\n';
+  }
+
+  cout << "the sum is " << val1+val2 << '\n';
+  cout << "the difference is " << val1-val2 << '\n';
+  cout << "the product is " << val1*val2 << '\n';
+  cout << "the quotient is " << val1/val2 << '\n';
+
+  return 0;
+}
