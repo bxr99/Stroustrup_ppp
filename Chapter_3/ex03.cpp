c65566303414fa9b938a7838d8d73baae79e2de3
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-02 19:15:12 -0400

    chapter 3 exercise 3

---
+++ Chapter_3/ex03.cpp
@@ -0,0 +1,11 @@
+#include "std_lib_facilities.h"
+
+// Program doesn't do anything, but declares a number of variables
+// with legal and illegal names to see how the compiler reacts
+int main()
+{
+  int double = 0;
+  string name = 17;
+  float size = 'a';
+  char q = 'q';
+}
