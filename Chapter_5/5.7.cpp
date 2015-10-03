b3d4fb4c37baed5d9d4f0da721cfbdb218b67fe5
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-03 18:43:26 -0400

    demo program with logic errors

---
+++ Chapter_5/5.7.cpp
@@ -0,0 +1,24 @@
+#include "std_lib_facilities.h"
+
+// demo logic errors
+int main()
+{
+  vector<double> temps;
+
+  for (double temp; cin>>temp; )
+    temps.push_back(temp);
+
+  double sum = 0;
+  double high_temp = 0;
+  double low_temp = 0;
+
+  for (int x : temps) {
+    if (x > high_temp) high_temp = x; // find high
+    if (x < low_temp) low_temp = x;   // find low
+    sum += x;                         // compute sum
+  }
+
+  cout << "High temperature: " << high_temp << '\n';
+  cout << "Low temperature: " << low_temp << '\n';
+  cout << "Average temperature: " << sum/temps.size() << '\n';
+}
