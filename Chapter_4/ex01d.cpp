d7b5c8eaec5dbedbdded584f4957eac210f3280d
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-03 12:46:31 -0400

    try-this exercise page 116

---
+++ Chapter_4/ex01d.cpp
@@ -0,0 +1,21 @@
+#include "std_lib_facilities.h"
+
+// Implementation of square() without using the multiplication operator
+// This program doesn't handle negative integer inputs
+int square(int n)
+{
+  int res = n;
+  for (int i = 1; i < n; ++i) {
+    res += n;
+  }
+  return res;
+}
+
+int main()
+{
+  int n;
+  cout << "Enter an integer to square: ";
+  cin >> n;
+  cout << n << " * " << n << " = " << square(n) << '\n';
+  return 0;
+}
