a88b1c311fa9fb5c4bde661d698e148cd6edd957
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-03 22:27:30 -0400

    chapter 5 drill

--- Chapter_5/drill.cpp
+++ Chapter_5/drill.cpp
@@ -8,24 +8,70 @@ int main()
   try {
     //--------------------------------------------------------------------------
 
-    //Cout << "Success!\n"; // Cout instead of cout
+    Cout << "Success!\n"; // Cout instead of cout
 
-    //cout << "Success!\n;  // missing "
+    cout << "Success!\n;  // missing "
 
-    //cout << "Success"  << !\n" // 3 errors here (! ; ")
+    cout << "Success"  << !\n" // 3 errors here (! ; ")
 
-    //cout << success << '\n'; // 'success' not declared in scope
+    cout << success << '\n'; // 'success' not declared in scope
 
-    //string res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n";
-    // Solution 1:
-    //string res = "7"; vector<string> v(10); v[5] = res; cout << "Success!\n";
-    // Solution 2:
-    //int res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n";
+    string res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n";
+    string res = "7"; vector<string> v(10); v[5] = res; cout << "Success!\n"; // Solution 1
+    int res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n";         // Solution 2
 
-    //vector<int> v(10); v(5) = 7; if (v(5)!=7) cout << "Success!\n";
-    //vector<int> v(10); v[5] = 7; if (v[5]==7) cout << "Success!\n";
+    vector<int> v(10); v(5) = 7; if (v(5)!=7) cout << "Success!\n";   // bad
+    vector<int> v(10); v[5] = 7; if (v[5] == 7) cout << "Success!\n"; // fixed
 
+    if (cond) << cout << "Success!\n"; else cout << "Fail!\n";                // bad
+    bool cond = true; if (cond) cout << "Success!\n"; else cout << "Fail!\n"; // fixed
 
+    string s = "ape"; boo c = "fool"<s; if (c) cout << "Success!\n";      // bad
+    string s = "ape"; bool c = (s < "fool"); if (c) cout << "Success!\n"; // fixed
+
+    string s = "ape"; if (s == "fool") cout << "Success!\n"; // bad
+    string s = "ape"; if (s != "fool") cout << "Success!\n"; // fixed
+
+    string s = "ape"; if (s+"fool") cout << "Success!\n"; // bad
+    string s = "ape"; if (s<"fool") cout << "Success!\n"; // fixed
+
+    vector<char> v(5); for (int i=0; 0<v.size(); ++i); cout << "Success!\n"; // bad
+    vector<char> v(5); for (int i=0; i<v.size(); ++i); cout << "Success!\n"; // fixed
+
+    vector<char> v(5); for (int i=0; i<=v.size(); ++i) cout << "Success!\n"; // bad
+    vector<char> v(5); for (int i=0; i<v.size(); ++i); cout << "Success!\n"; // fixed
+
+    string s = "Success!\n"; for (int i=0; i<6; ++i) cout << s[i]; //bad
+    string s = "Success!\n"; for (int i=0; i<9; ++i) cout << s[i]; //fixed
+
+    if (true) then cout << "Success!\n"; else cout << "Fail!\n"; //bad
+    if (true) cout << "Success!\n"; else cout << "Fail!\n";      //fixed
+
+    int x = 2000; char c = x; if (c==2000) cout << "Success!\n"; //bad;
+    int x = 2000; char c = x; if (c!=2000) cout << "Success!\n"; //fixed
+
+    string s = "Success!\n"; for (int i = 0; i <10; ++i) cout << s[i]; // prints "Success!" plus some blank characters
+
+    vector v(5); for (int i = 0; i <= v.size(); ++i); cout << "Success!\n";      //bad
+    vector<int> v(5); for (int i = 0; i <= v.size(); ++i); cout << "Success!\n"; // fixed
+
+    int i=0; int j=9; while (i<10) ++j; if (j<i) cout << "Success!\n"; //bad
+    int i=0; int j=9; while (i>10) ++j; if (j>i) cout << "Success!\n"; //fixed
+
+    int x = 2; double d = 5/(x-2); if (d==2*x+0.5) cout << "Success!\n"; // bad
+    int x = 2; double d = 5/(x+2); if (d<=2*x+0.5) cout << "Success!\n"; //fixed
+
+    string<char> s = "Success!\n"; for (int i=0; i<=10; ++i) cout << s[i]; //bad
+    string s = "Success!\n"; for (int i=0; i<=10; ++i) cout << s[i];       //fixed
+
+    int i=0; while (i<10) ++j; if (j<i) cout << "Success!\n";          //bad
+    int i=0; int j=0; while (j<10) ++j; if (j>i) cout << "Success!\n"; //fixed
+
+    int x = 4; double d = 5/(x-2); if (d=2*x+0.5) cout << "Success!\n";  //bad
+    int x = 4; double d = 5/(x-2); if (d<=2*x+0.5) cout << "Success!\n"; //fixed
+
+    cin << "Success!\n";  // bad
+    cout << "Success!\n"; // fixed
 
     //--------------------------------------------------------------------------
     return 0;
