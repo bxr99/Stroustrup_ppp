d68da79fcf9749cf77cb8698a4616b7b1d41d705
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-04 11:55:21 -0400

    chapter 5 exercise 8

---
+++ Chapter_5/ex08.cpp
@@ -0,0 +1,23 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+  vector<int> values;
+  int N = 0;
+
+  cout << "Enter the number of values you want to sum: ";
+  cin >> N;
+
+  cout << "Enter some integers (press '|' to stop): ";
+  for (int i; cin>>i;)
+    values.push_back(i);
+
+  int sum = 0;
+  for (int i = 0; i < N; ++i) sum += values[i];
+
+  cout << "The sum of the first " << N << " numbers ( "; //  << sum << '\n';
+  for (int i = 0; i < N; ++i) cout << values[i] << " ";
+  cout << ") is " << sum << '\n';
+
+  return 0;
+}
