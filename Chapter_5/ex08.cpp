fbd24a7126b77f8fbf94b95d02fbe42cad98e21a
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-04 12:10:56 -0400

    chapter 5 exercise 8, handles error case now

--- Chapter_5/ex08.cpp
+++ Chapter_5/ex08.cpp
@@ -2,22 +2,30 @@
 
 int main()
 {
-  vector<int> values;
-  int N = 0;
+  try {
+    vector<int> values;
+    int N = 0;
 
-  cout << "Enter the number of values you want to sum: ";
-  cin >> N;
+    cout << "Enter the number of values you want to sum: ";
+    cin >> N;
 
-  cout << "Enter some integers (press '|' to stop): ";
-  for (int i; cin>>i;)
-    values.push_back(i);
+    cout << "Enter some integers (press '|' to stop): ";
+    for (int i; cin>>i;)
+      values.push_back(i);
 
-  int sum = 0;
-  for (int i = 0; i < N; ++i) sum += values[i];
+    int sum = 0;
+    for (int i = 0; i < N; ++i) sum += values[i];
 
-  cout << "The sum of the first " << N << " numbers ( ";
-  for (int i = 0; i < N; ++i) cout << values[i] << " ";
-  cout << ") is " << sum << '\n';
+    cout << "The sum of the first " << N << " numbers ( ";
+    for (int i = 0; i < N; ++i) cout << values[i] << " ";
+    cout << ") is " << sum << '\n';
 
-  return 0;
+    return 0;
+  } catch (out_of_range) {
+    cerr << "Error: asked for a sum of more numbers than there are in the vector\n";
+    return 1;
+  } catch (...) { // catch all other exceptions
+    cerr << "something went wrong\n";
+    return 2;
+  }
 }
