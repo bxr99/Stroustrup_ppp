2c3b1a5a8479e1eac7089547bc47af80036f3617
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-05 15:54:48 -0400

    making progress

--- Chapter_5/ex13.cpp
+++ Chapter_5/ex13.cpp
@@ -1,25 +1,52 @@
 #include "std_lib_facilities.h"
 
+vector<int> get_v()
+{
+  vector<int> v;
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+  v.push_back(4);
+  return v;
+}
+
+void print_v(vector<int> v)
+{
+  for (int i = 0; i < v.size(); ++i)
+    cout << v[i] << " ";
+}
+
 int main()
 {
-  vector<int> values = {1,2,3,4};
+  vector<int> res = get_v();
   vector<int> guesses;
-  bool go = true;
-  int guess = 0;
 
-  while (go) {
-    int count = 4;
-      for (int guess; cin>>guess; ) {
-        guesses.push_back(guess);
-        --count;
-        if (count == 0) break;
-      }
+  for (int guess; cin>>guess; ) {
+    guesses.push_back(guess);
+    if (guesses.size() == 4) break;
+  }
 
-    if (guesses.size() == 4) {
-      for (int i = 0; i < guesses.size(); ++i)
-        cout << guesses[i] << " ";
-      go = false;
-    }
+  int count = 0;
+  for (int i = 0; i < guesses.size(); ++i) {
+    if (guesses[i] == res[i]) ++count;
+  }
+
+  switch (count) {
+    case 4:
+      cout << "four bulls";
+      break;
+    case 3:
+      cout << "three bulls one cow";
+      break;
+    case 2:
+      cout << "two bulls two cows";
+      break;
+    case 1:
+      cout << "one bull three cows";
+      break;
+    default:
+      cout << "four cows";
+      break;
   }
 
   cout << '\n';
