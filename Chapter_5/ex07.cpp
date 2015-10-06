c7d7c1a6ff609280efca40865c20015e21e134e5
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-05 20:31:23 -0400

    chapter 5 exercise 7

--- Chapter_5/ex07.cpp
+++ Chapter_5/ex07.cpp
@@ -17,13 +17,17 @@ int main()
     cin >> b;
     cout << "Enter c: ";
     cin >> c;
-    cout << "x = " << root1(a,b,c) << " or x = " << root2(a,b,c) << '\n';
+    if (root1(a,b,c) == root2(a,b,c)) {
+      cout << "There is only one root, x = " << root1(a,b,c) << '\n';
+    } else {
+      cout << "x = " << root1(a,b,c) << " or x = " << root2(a,b,c) << '\n';
+    }
     return 0;
-  } catch (runtime_error   &e) {
-    cerr << e.what() << '\n';
+  } catch (exception &e) {
+    cerr << "Exception Caught: " << e.what() << '\n';
     return 1;
   } catch (...) {
-    cerr << "unknown exception\n";
+    cerr << "Unknown Exception\n";
     return 2;
   }
 }
@@ -32,20 +36,20 @@ int main()
 // pre-condition 2: if discriminant is negative, no real roots exist
 double root1(double a, double b, double c)
 {
-  if (a == 0) error("division by zero\n");
-  if ((b*b-4*a*c) < 0) error("discriminant is negative\n");
+  if (a == 0) error("Division by zero");
+  if ((b*b-4*a*c) < 0) error("Discriminant is negative");
 
   double disc = b*b-4*a*c;
-  return (-b + sqrt(disc))/2*a;
+  return (-b + sqrt(disc))/(2*a);
 }
 
 // pre-condition 1: a >= 1
 // pre-condition 2: if discriminant is negative, no real roots exist
 double root2(double a, double b, double c)
 {
-  if (a == 0) error("division by zero\n");
-  if ((b*b-4*a*c) < 0) error("discriminant is negative\n");
+  if (a == 0) error("Division by zero");
+  if ((b*b-4*a*c) < 0) error("Discriminant is negative");
 
   double disc = b*b-4*a*c;
-  return (-b - sqrt(disc))/2*a;
+  return (-b - sqrt(disc))/(2*a);
 }
