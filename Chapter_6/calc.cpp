870721059a969ac3f54bdd98423fc8293d6b6d20
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-06 10:44:23 -0400

    basicCalc version 2, does not handle operator precedence (e.g. PEMDAS)

---
+++ Chapter_6/calc.cpp
@@ -0,0 +1,37 @@
+#include "std_lib_facilities.h"
+
+// slightly improved calculator as it handles longer expressions, however, this
+// does not handle operator precedence (e.g. PEMDAS)
+int main()
+{
+  int lval = 0;
+  int rval = 0;
+  char op;
+
+  cout << "Please enter expression\n";
+  cout << "add an 'e' to the end expression (e.g. 1+2*3 e): ";
+  cin >> lval;
+
+  if (!cin) error("No first operand");
+  for (char op; cin>>op; ) {
+    if (op != 'e') cin >> rval;
+    switch (op) {
+      case '+':
+        lval += rval;
+        break;
+      case '-':
+        lval -= rval;
+        break;
+      case '*':
+        lval *= rval;
+        break;
+      case '/':
+        lval /= rval; // Note: still integer division
+        break;
+      default:
+        cout << "Result: " << lval << '\n';
+        return 0;
+    }
+  }
+  error("Bad expression");
+}
