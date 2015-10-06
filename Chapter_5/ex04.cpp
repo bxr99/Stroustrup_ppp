0dee84c75f391561c678f10a347ec0b979f833d5
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-05 20:13:00 -0400

    chapter 5 exercise 4

--- Chapter_5/ex04.cpp
+++ Chapter_5/ex04.cpp
@@ -1,1 +1,26 @@
 #include "std_lib_facilities.h"
+
+double c_to_k(double c)
+{
+  if (c < -273.15) error("Entered a value less than 0k");
+  double k = c + 273.15;
+  return k;
+}
+
+int main()
+{
+  try {
+    double c;
+    cout << "Enter Celsius: ";
+    cin >> c;
+    double k = c_to_k(c);
+    cout << c << " Celsius is " << k << " Kelvin" << '\n';
+    return 0;
+  } catch (exception &e) {
+    cout << "Exception Caught: " << e.what() << '\n';
+    return 1;
+  } catch (...) {
+    cout << "Unknown Exception";
+    return 2;
+  }
+}
