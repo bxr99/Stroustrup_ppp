592c20fa4dcda68a71dd0112ca69ad6f15aa5ce5
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-10 12:29:24 -0400

    C++ programs using the Code::Blocks IDE

---
+++ CodeBlocks/IDE_Test/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void print_v(vector<int> v)
+{
+	for (int i : v) { // for each 'i' in vector 'v', do stuff..
+		cout << i << " ";
+	}
+	cout << '\n';
+}
+
+int main()
+{
+	vector<int> values;
+	for (int i = 0; i < 10; ++i)
+		values.push_back(i);
+
+	print_v(values);
+	return 0;
+}
