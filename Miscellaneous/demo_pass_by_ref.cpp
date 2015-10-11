81fbbd778fcc7f21241c660868e9194f1b22500d
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-11 10:01:25 -0400

    passing arguments by reference

---
+++ Miscellaneous/demo_pass_by_ref.cpp
@@ -0,0 +1,27 @@
+// demo pass-by-reference
+#include "std_lib_facilities.h"
+
+void duplicate(int& a, int& b, int& c)
+{
+  a *= 2;
+  b *= 2;
+  c *= 2;
+}
+
+int main()
+{
+  int x = 1;
+  int y = 3;
+  int z = 7;
+
+  // before
+  cout << "x= " << x << ", y= " << y << ",z= " << z;
+  cout << '\n';
+
+  duplicate(x,y,z);
+
+  // after
+  cout << "x= " << x << ", y= " << y << ",z= " << z;
+  cout << '\n';
+  return 0;
+}
