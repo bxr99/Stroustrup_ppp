4b89c1399801c1b504cd46745fc98ba1baa94fae
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-12 18:18:35 -0400

    chapter 8, problem 2

--- Chapter_8/drill_2.cpp
+++ Chapter_8/drill_2.cpp
@@ -6,6 +6,25 @@ void swap_cr(const int&, const int&);
 
 int main()
 {
+  //int x = 7;
+  //int y = 9;
+  //swap_v(x,y);
+  //cout << x << " " << y << '\n'; // 7 9
+  //swap_r(x,y);
+  //cout << x << " " << y << '\n'; // 9 7
+
+  //const int cx = 7;
+  //const int cy = 9;
+  //swap_v(cx,cy); // 7 9
+  //swap_r(cx,cy); // Error: reference of type int, got const int
+  //cout << cx << " " << cy << '\n';
+
+  //double dx = 7.7;
+  //double dy = 9.9;
+  //swap_v(dx,dy);
+  //cout << dx << " " << dy << '\n'; // 7.7 9.9
+  //swap_r(dx,dy);// Error: expected reference to type int, got type double
+  //cout << dx << " " << dy << '\n';
 
   return 0;
 }
