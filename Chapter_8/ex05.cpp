1d628a3bc2535ab9487091613c421f325ab9c194
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-12 20:58:24 -0400

    chapter 8, exercise 5

---
+++ Chapter_8/ex05.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+// prototypes
+std::vector<int> reverse_1(const std::vector<int>);
+void reverse_2(std::vector<int>&);
+void swap(int&, int&);
+void print_v(std::vector<int>);
+
+int main()
+{
+  std::vector<int> v1 = {1,3,5,7,9};
+  reverse_2(v1);
+  print_v(v1);
+  return 0;
+}
+
+// read and write access only
+void print_v(const std::vector<int> v)
+{
+  std::cout << "{ ";
+  for (int i = 0; i < v.size(); ++i) {
+    std::cout << v[i];
+    if (i != v.size()-1) std::cout << ", ";
+  }
+  std::cout << " }\n";
+}
+
+// first reverse function produces a new vector with the reversed sequence
+// leaving its original vector unchanged
+std::vector<int> reverse_1(const std::vector<int> v)
+{
+  std::vector<int> r;
+  for (int i = v.size()-1; i >= 0; --i)
+    r.push_back(v[i]);
+  return r;
+}
+
+// second reverse function reverses the elements of the vector without using
+// any other vectors (hint: swap)
+void reverse_2(std::vector<int>& v)
+{
+  int tmp = 0; // placeholder for index
+  for (int i = v.size()-1; i >= 0; --i) {
+    swap(v[tmp++],v[i]);
+  }
+}
+
+void swap(int& a, int& b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
