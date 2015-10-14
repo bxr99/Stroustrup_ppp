974d65c07f697237c4f860ef48299d9b9dd0a121
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-14 10:44:01 -0400

    still need to finish

--- Chapter_8/ex07.cpp
+++ Chapter_8/ex07.cpp
@@ -7,6 +7,21 @@
 void print_age(const std::vector<double>&);
 void print_name(const std::vector<std::string>&);
 void print_pair(const std::vector<std::string>&, const std::vector<double>&);
+void read_names(); // just 5 names, later change to arbitrary amount
+void read_pairs(); // read both names, and an age
+
+// read five names
+void read_names()
+{
+  std::vector<std::string> names;
+  std::string s = "";
+
+  for (int i = 0; i < 5; ++i) {
+    std::cin >> s;
+    names.push_back(s);
+  }
+}
+
 
 void print_age(const std::vector<double>& v)
 {
@@ -28,7 +43,20 @@ void print_name(const std::vector<std::string>& v)
   std::cout << " }\n";
 }
 
-void print_pair(const std::vector<std::string>& names, const std::vector<double>& ages)
+void read_pairs(std::vector<std::string>& names, std::vector<double>& ages)
+{
+  std::string name = "";
+  double age = 0;
+  for (int i = 0; i < 5; ++i) {
+    std::cout << "Type a name and an age (separated by <space>): ";
+    std::cin >> name >> age;
+    names.push_back(name);
+    ages.push_back(age);
+  }
+  std::cout << std::endl;
+}
+
+void print_pairs(const std::vector<std::string>& names, const std::vector<double>& ages)
 {
   if (names.size() != ages.size()) {
     throw std::invalid_argument("Vectors must be the same size");
@@ -42,24 +70,14 @@ void print_pair(const std::vector<std::string>& names, const std::vector<double>
 int main()
 {
   try {
+
     std::vector<std::string> original_names;
-    std::vector<double> original_ages;
-    std::string s = "";
-    double age = 0;
+    std::vector<double> orignal_ages;
 
-    for (int i = 0; i < 5; ++i) {
-      std::cout << "Enter a name (followed by <enter>): ";
-      std::cin >> s;
-      original_names.push_back(s);
-    }
+    read_pairs(original_names, orignal_ages);
+    print_pairs(original_names, orignal_ages);
 
-    for (int i = 0; i < 5; ++i) {
-      std::cout << "Enter the age of " << original_names[i] << ": ";
-      std::cin >> age;
-      original_ages.push_back(age);
-    }
-
-    // TODO: add more stuff
+    // TODO: make copies of stuff
 
     return 0;
   } catch (const std::invalid_argument& e) {
