decb89f026f515279e89a97c8f29c5b8bb8e5297
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-15 22:56:27 -0400

    more random stuff on stucts

---
+++ Miscellaneous/more_structs.cpp
@@ -0,0 +1,13 @@
+struct Product {
+  int weight;
+  double price;
+};
+
+Product apple;
+Product banana, melon;
+
+// Another way to declare a struct
+struct Product {
+  int weight;
+  double price;
+} apple, banana, melon;
