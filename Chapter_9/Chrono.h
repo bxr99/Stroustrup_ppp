6fa8711340c853c333f4fd5541e0347fb6e420d4
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-19 16:33:35 -0400

    from section 9.8

---
+++ Chapter_9/Chrono.h
@@ -0,0 +1,42 @@
+namespace Chrono {
+
+  enum class Month {
+    jan=1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+  };
+
+  class Date {
+  public:
+    class Invalid {}; // to throw as exception
+
+    Date(int m, int d, int y); // check for valid date and initialize
+    Date();                    // default constructor
+    // default copy operations are fine
+
+    // nonmodifying operations
+    int day() const { return d; }
+    Month month() const { return m; }
+    int year() const { return y; }
+
+    // modifying operations
+    void add_day(int n);
+    void add_month(int n);
+    void add_year(int n);
+
+  // format mo/day/year
+  private:
+    Month m;
+    int d;
+    int y;
+  };
+
+  bool is_date(Month m, int d, int y); // true for valid date
+  bool leapyear(int y); // true if y is a leap year
+  bool operator==(const Date& a, const Date& b);
+  bool operator!=(const Date& a, const Date& b);
+
+  ostream& operator<<(ostream& os, const Date& d);
+
+  istream& operator>>(istream& is, Date& dd);
+
+} // Chrono
+
