3a23ee465ed4f0ec8bd5c8385ba912966a7320c2
Eli Bregman <bmajor99@terpmail.umd.edu>
2015-10-21 10:48:39 -0400

    chapter 9, exercise 2 (not finished)

---
+++ Chapter_9/ex02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+class Name_pairs {
+public:
+  std::vector<std::string> read_names(std::vector<std::string>& names)
+  {
+    int count = 0;
+    std::cout << "How many names do you want to enter: ";
+    std::cin >> count;
+
+    std::string s = "";
+    for (int i = 0; i < count; ++i) {
+      std::cout << "Enter name (followed by <enter>): ";
+      std::cin >> s;
+      names.push_back(s);
+    }
+    return names;
+  }
+private:
+  std::vector<std::string> name_;
+  std::vector<double> age_;
+};
+
